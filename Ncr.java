ee4c2b226e768fcef1df993853af26d6191bcd33
Ujjwal kumar <102848863+kUjjwal69@users.noreply.github.com>
2022-10-01 15:17:38 +0530

    Update Ncr.java

--- Ncr.java
+++ Ncr.java
@@ -40,4 +40,32 @@ int n=10,r=6;
     int ncr=factn/(factr*factnr);
     System.out.print(ncr);
 }
+// approach 3 using methods
+
+
+import java.util.Scanner;
+public class NcR {
+    public static int factorial(int num){
+        int fact=1;
+        for(int i=1;i<=num;i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
+    public static int ncr(int n,int r){
+        int factN=factorial(n);
+        int factR=factorial(r);
+        int factNR=factorial(n-r);
+        int result=factN/(factR*factNR);
+        return result;
+    }
+    public static void main(String args[]) {
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     int r=s.nextInt();
+     
+     int result=ncr(n,r);
+     System.out.println(result);
+    }
+}
     
